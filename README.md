144fdeb9fb5fe2dfb479e7b74cd96b4e499aaaea
HenryJ <henryjung667@gmail.com>
2020-10-01 20:20:02 -0500

    added to README and added comments

---
+++ README.md
@@ -0,0 +1,23 @@
+# Password Generator homework
+
+## description
+
+My password generator gives you the options of Uppercase and Lowercase letters, numbers, and symbols. The generator gives you a list of prompts including a length prompt where you must enter a value between 8-128, anything outside of that peramiter will result in the generator prompting you to try again.
+
+## Links 
+
+Github Repo: https://github.com/Henryrjung/03_password_generator 
+
+Git pages website: https://henryrjung.github.io/03_password_generator/
+
+## Screenshot 
+![Image 1](./assets/03_password_generator_screenshot01.PNG)
+
+## issues 
+With this homework I mainly had problems with syntax, and understanding how things connected to each other, primarily how the strings of number,sybol etc... is used by the function.
+
+## Credits
+
+1. class repo
+1. "creating a random password generator" by Nazmus Nasir on youtube(His video really helped me understand how the script all works together.)
+1. w3 schools 
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// all characters
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var symbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
@@ -18,7 +18,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+//function to generate password
 function generatePassword() {
   var passLength = prompt("Please enter a length between 8 and 128"); {
     if (passLength >= 8 && passLength <= 128) {
@@ -28,31 +28,31 @@ function generatePassword() {
       var passLength = prompt("Please enter a length between 8 and 128");
     }
   }
-
+  //confirms that will pop up on clicking generate password button
   var askNum = confirm("Do you want numbers?"); 
   var askSym = confirm("Do you want symbols");
   var askUpper = confirm("do you want uppercase letters");
   var askLower = confirm("do you want lowecase letters");
 
   var characters = []
-
+  // numbers
   if (askNum) {
     characters = characters.concat(numbers) 
-    
+    //symbols
   } if (askSym) {
     characters = characters.concat(symbols)  
-
+    //uppercase 
   }  if (askUpper) {
     characters = characters.concat(upperCase)
-
+    //lowercase
   }  if (askLower) {
     characters = characters.concat(lowerCase)
 
   }
   var generatedPassword = "";
-
+  //for loop to combine selected characters
   for (var i = 0; i < passLength; i++) {
-    generatedPassword = generatedPassword + characters[Math.floor(Math.random() * characters.length)];
+    generatedPassword += characters[Math.floor(Math.random() * characters.length)];
   }
   return generatedPassword;
 } 
